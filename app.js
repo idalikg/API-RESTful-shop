b2898fa3d35ea3e47649339d45034450cd885bb8
Dalia Galván <idalikg@gmail.com>
2018-01-07 13:13:32 -0600

    Mongoose re-Connection En 'app.js' se reestructuró la conexión con la Base de Datos; específicamente se declaró un variable con la URI de la BD de mongoDB, y posteriormente se implementó el uso Promesas para que además del método 'connect', con el método 'then' también sea posible verificar si la conexión es exitosa.

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const routeProduct = require('./routes/products');
 const routeOrder = require('./routes/orders');
 
 // Mongoose conection
-mongoose.connect("mongodb://node-shop:"+process.env.MONGO_ATLAS_PWD +"@node-rest-shop-shard-00-00-umxws.mongodb.net:27017,node-rest-shop-shard-00-01-umxws.mongodb.net:27017,node-rest-shop-shard-00-02-umxws.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin");
+// variable con la dirección hacia la base de datos
+const uri = `mongodb://node-shop:${process.env.MONGO_ATLAS_PWD}@node-rest-shop-shard-00-00-umxws.mongodb.net:27017,node-rest-shop-shard-00-01-umxws.mongodb.net:27017,node-rest-shop-shard-00-02-umxws.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin`;
+
+// esableciento de la conexión entre la API y la DB
+mongoose.connect(uri).then(
+    () => { console.log('Conexión establecida con la BD de mongoDB Atlas') },
+    err => { console.log('Error de conexión con la BD') }
+);
 
 // INIT APP
 const app = express();
